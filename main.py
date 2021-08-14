197efb6ab911f97720a3b7724685849ba48d7119
elainealverina <elio0003@student.monash.edu>
2021-08-14 18:58:06 +0700

    Import splitfolders library and wrote initial code for splitting dataset

--- main.py
+++ main.py
@@ -16,4 +16,16 @@ import torchvision
 from torchvision import *
 from torch.utils.data import Dataset, DataLoader
 
+import splitfolders
+
 # Dividing Dataset 70-30
+## input_folder: coad_msi_mss
+def img_train_test_split(input_folder, output_folder):
+    """
+    This function splits a folder with subfolders into train and test datasets
+    :param input_folder: a string: file path of the folder of subfolders of images
+    :param output_folder: a string: path to the output folder
+    :return: -
+    """
+    splitfolders.ratio(input_folder, output_folder, seed=0, ratio=(.7, .3))
+    return None
