8cf47d032a193d46cef6db1eb222084c06019d3b
vionnietan <vtan0015@student.monash.edu>
2021-08-14 15:29:16 +0700

    Importing Libraries for Main Program

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 # Main Program file for Cancer Predictive Model
 # By: Group 4
 
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os, random, shutil
+import copy
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+from torchvision import *
+from torch.utils.data import Dataset, DataLoader
+
+# Dividing Dataset 70-30
